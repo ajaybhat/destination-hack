ea9e0f1e8832276e9dc20772a2ef1c880eee145a
Ajay Bhat <a.ajay.bhat@gmail.com>
2015-07-25 15:11:19 +0530

    Rough

--- destination-hack.py
+++ destination-hack.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/destihack/putuser', methods=['POST'])
+def put():
+    json_string = request.get_json(force=True)
+    print json_string
+    return ''   
 
+
+@app.route('/destihack/newuser')
+def newuser():
+    user = {'name': 'Ajay', 'surname': 'Bhat', 'age': '22'}
+    return jsonify(user)
+
+
+@app.route('/destihack/')
+@app.route('/destihack/index')
+def index():
+    return 'Hi. Destihack webapp aanu ithu'
+
+@app.route('/')
+def xx():
+    return 'Hi'
 
 if __name__ == '__main__':
     app.run()
