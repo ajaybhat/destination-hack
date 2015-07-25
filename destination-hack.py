86dd98c98d94ccb9baef1035dd8880687b8429a7
Ajay Bhat <a.ajay.bhat@gmail.com>
2015-07-25 19:29:58 +0530

    refactor

--- destination-hack.py
+++ destination-hack.py
@@ -1,11 +1,9 @@
-from flask import Flask, jsonify, request, g, render_template
-from sqlalchemy import create_engine
-from model import connect_db, create_user, get_all_users, get_user
-from json import JSONEncoder, JSONDecoder, dumps
+from json import dumps
+from flask import Flask, request
 from uuid import uuid4
+from model import create_user, get_all_users, get_user
 
 app = Flask(__name__)
-
 
 @app.route('/destihack/getallusers')
 def getusers():
@@ -23,13 +21,6 @@ def newuser():
     user = create_user(int(uuid4().int >> 70), incoming_user['gid'], incoming_user['fname'],
                        incoming_user['lname'], incoming_user['email'], incoming_user['gender'])
     return dumps(user)
-
-
-@app.route('/destihack/')
-@app.route('/destihack/index')
-def index():
-    return 'Hi. Destihack webapp aanu ithu'
-
 
 if __name__ == '__main__':
     app.run()
--- model.py
+++ model.py
@@ -8,34 +8,49 @@ def connect_db():
     return connect("destination.db")
 
 
-def create_user(uid, gid, fname, lname, email, gender):
+def execute_query(query):
     engine = create_engine('sqlite:///destination.db')
     connection = engine.connect()
+    results = connection.execute(query)
+    connection._commit_impl(autocommit=True)
+    return results
+
+
+def create_user(uid, gid, fname, lname, email, gender):
     user = get_user(gid)
     if user == None:
         query = "INSERT INTO users VALUES ({},{},{},{},{},{})".format(int(uid), gid, fname, lname, gender, email)
-        results = connection.execute(query)
-        connection._commit_impl(autocommit=True)
+        results = execute_query(query)
         results.close()
         return {"user": get_user(uid), "exists": False}
     return {"user": user, "exists": True}
 
 
 def get_user(uid):
-    engine = create_engine('sqlite:///destination.db')
-    connection = engine.connect()
-    results = connection.execute("select * from users where uid={}".format(uid))
-    connection._commit_impl(autocommit=True)
+    results = execute_query("select * from users where uid={}".format(uid))
     user = results._fetchone_impl()
     results.close()
     return user
 
 
 def get_all_users():
-    engine = create_engine('sqlite:///destination.db')
-    connection = engine.connect()
-    results = connection.execute("select * from users")
-    connection._commit_impl(autocommit=True)
+    results = execute_query("select * from users")
     users = results._fetchall_impl()
     results.close()
     return users
+
+
+def get_place_reviews(place_id):
+    results = execute_query("SELECT * FROM reviews WHERE place_id={}".format(place_id))
+    reviews = results._fetchall_impl()
+    results.close()
+    return reviews
+
+
+def get_places_visited(uid):
+    results = execute_query("SELECT * FROM visited WHERE uid={}".format(uid))
+    places = results._fetchall_impl()
+    results.close()
+    return places
+
+
