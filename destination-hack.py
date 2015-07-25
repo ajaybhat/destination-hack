b2b32f07e9028448e8b83e35b98ed4bc3ace1a08
Ajay Bhat <a.ajay.bhat@gmail.com>
2015-07-25 19:12:50 +0530

    Refactor

--- destination-hack.py
+++ destination-hack.py
@@ -1,43 +1,28 @@
 from flask import Flask, jsonify, request, g, render_template
 from sqlalchemy import create_engine
-from model import connect_db, create_user, query_user
-from json import JSONEncoder, JSONDecoder
+from model import connect_db, create_user, get_all_users, get_user
+from json import JSONEncoder, JSONDecoder, dumps
 from uuid import uuid4
 
 app = Flask(__name__)
 
 
-@app.before_request
-def set_up_db():
-    g.db = connect_db()
+@app.route('/destihack/getallusers')
+def getusers():
+    return dumps({"users": get_all_users()})
 
 
-@app.teardown_request
-def close_db(e):
-    g.db.close()
-
-@app.route('/destihack/showusers')
-def show_entries():
-    pass
-  #  engine = create_engine('sqlite:///destination.db')
-  #  connection = engine.connect()
-  #  results = connection.execute("select * from users")
-  #  connection._commit_impl(autocommit=True)
-  #  x = [row for row in results]
-  #  print x[0]
-  #  results.close()
-  #  return jsonify(x[0])
+@app.route('/destihack/get_user')
+def getuser():
+    return dumps({"user": get_user(request.args['uid'])})
 
 
-@app.route('/destihack/newuser', methods=['POST'])
+@app.route('/destihack/login', methods=['POST'])
 def newuser():
     incoming_user = request.get_json(force=True)
-    user = query_user(g.db, incoming_user['gid'])
-    if user == None:
-        user = create_user(g.db, int(uuid4().int >> 70), incoming_user['gid'], incoming_user['fname'],
-                           incoming_user['lname'],
-                           incoming_user['email'], incoming_user['gender'])
-    return ''
+    user = create_user(int(uuid4().int >> 70), incoming_user['gid'], incoming_user['fname'],
+                       incoming_user['lname'], incoming_user['email'], incoming_user['gender'])
+    return dumps(user)
 
 
 @app.route('/destihack/')
--- model.py
+++ model.py
@@ -1,21 +1,41 @@
 from sqlite3 import connect
 import datetime
+from sqlalchemy import create_engine
+from json import dumps
+
 
 def connect_db():
-	return connect("destination.db")
-
-def create_user(db, uid, gid, fname, lname, email, gender):
-    c = db.cursor()
-    query = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)"
-    c.execute(query, (uid, gid, fname, lname, email, gender))
-    db.commit()
-    return query_user(gid)
+    return connect("destination.db")
 
 
+def create_user(uid, gid, fname, lname, email, gender):
+    engine = create_engine('sqlite:///destination.db')
+    connection = engine.connect()
+    user = get_user(gid)
+    if user == None:
+        query = "INSERT INTO users VALUES ({},{},{},{},{},{})".format(int(uid), gid, fname, lname, gender, email)
+        results = connection.execute(query)
+        connection._commit_impl(autocommit=True)
+        results.close()
+        return {"user": get_user(uid), "exists": False}
+    return {"user": user, "exists": True}
 
-def query_user(db, gid):
-    c = db.cursor()
-    query = "SELECT * FROM users where gid = (?)"
-    result = c.execute(query,(gid))
-    db.commit()
-    return result.fetchone()
+
+def get_user(uid):
+    engine = create_engine('sqlite:///destination.db')
+    connection = engine.connect()
+    results = connection.execute("select * from users where uid={}".format(uid))
+    connection._commit_impl(autocommit=True)
+    user = results._fetchone_impl()
+    results.close()
+    return user
+
+
+def get_all_users():
+    engine = create_engine('sqlite:///destination.db')
+    connection = engine.connect()
+    results = connection.execute("select * from users")
+    connection._commit_impl(autocommit=True)
+    users = results._fetchall_impl()
+    results.close()
+    return users
