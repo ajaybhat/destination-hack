d6d473b964cb8aaaf01f21969be2ac1525628cab
Ajay Bhat <a.ajay.bhat@gmail.com>
2015-07-25 17:07:18 +0530

    Refactor

--- destination-hack.py
+++ destination-hack.py
@@ -1,29 +1,8 @@
 from flask import Flask, jsonify, request, g, render_template
-from sqlite3 import connect
-from contextlib import closing
+from sqlalchemy import create_engine
 
-SCHEMA = 'schema.sql'
-DATABASE = 'destination.db'
 app = Flask(__name__)
 
-def connect_db():
-    return connect(DATABASE)
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource(SCHEMA, mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
 
 @app.route('/destihack/putuser', methods=['POST'])
 def put():
@@ -36,17 +15,25 @@ def put():
 def getsomedb():
     return 'Done'
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
+
 @app.route('/destihack/showusers')
 def show_entries():
-    cur = g.db.execute('select * from users')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    engine = create_engine('sqlite:///destination.db')
+    connection = engine.connect()
+    results = connection.execute("select * from users where uid=1")
+    connection._commit_impl(autocommit=True)
+    x = [row for row in results]
+    print x[0]
+    results.close()
+    return x[0]
+
 
 @app.route('/destihack/newuser')
 def newuser():
@@ -58,6 +45,7 @@ def newuser():
 @app.route('/destihack/index')
 def index():
     return 'Hi. Destihack webapp aanu ithu'
+
 
 if __name__ == '__main__':
     app.run()
